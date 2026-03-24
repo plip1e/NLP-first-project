7fa597a028ea5a22f37ba1f2f952c4d2a24b4878
Paul Du Preez <pau.dupreez04@gmail.com>
2026-03-24 21:28:32 +0100

    fixed bugs in clean_df function

--- func.py
+++ func.py
@@ -4,30 +4,27 @@ import pandas as pd
 def clean_df(df: pd.DataFrame, drop_columns: list = ['text']) -> pd.DataFrame:
 
     def add_reporter(df: pd.DataFrame) -> pd.DataFrame: # seporate reporter name from article content
-        clean_text = ''
         for index, row in df.iterrows():
-            text = row['text']
-            text = clean_text(text)
+            text: str = row['text']
             words = text.split()
             # check if ' - ' is within the first 5 words
-        if ' - ' in text and text.index(' - ') < len(' '.join(words[:5])):
-            df.at[index, 'reporter'] = text.split(' - ')[0]
-        else: df.at[index, 'reporter'] = "Unknown"
+            if ' - ' in text and text.index(' - ') < len(' '.join(words[:5])):
+                df.at[index, 'reporter'] = text.split(' - ')[0]
+            else: df.at[index, 'reporter'] = "Unknown"
         return df
     
     def add_content(df: pd.DataFrame) -> pd.DataFrame: # seporate article content from reporter name
         for index, row in df.iterrows():
-            text = row['text']
+            text: str = row['text']
             words = text.split()
             # check if ' - ' is within the first 5 words
-        if ' - ' in text and text.index(' - ') < len(' '.join(words[:5])):
-            df.at[index, 'content'] = text.split(' - ')[1]
-        else: df.at[index, 'content'] = text
+            if ' - ' in text and text.index(' - ') < len(' '.join(words[:5])):
+                df.at[index, 'content'] = text.split(' - ')[1]
+            else: df.at[index, 'content'] = text
         return df
     
     def drop_text(df: pd.DataFrame, drop_columns: list) -> pd.DataFrame: 
-        df = df.drop(columns=[drop_columns]) # drop the original 'text' column
-        return df
+        return df.drop(columns=drop_columns, inplace=False) # drop the original 'text' column
     
     df = add_reporter(df)
     df = add_content(df)
